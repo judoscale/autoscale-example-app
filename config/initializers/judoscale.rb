a7a94dc385f0048068a88dcb0f0f3cb3879a77c6
Adam McCrea <adam@adamlogic.com>
2023-08-22 17:11:28 -0400

    Handle judoscale-test and judoscale-staging addons
    The non-production add-ons set JUDOSCALE_STAGING_URL and JUDOSCALE_TEST_URL instead of JUDOSCALE_URL, but we still want to override the host.

--- config/initializers/judoscale.rb
+++ config/initializers/judoscale.rb
@@ -2,11 +2,16 @@ require 'uri'
 
 if defined?(Judoscale)
   Judoscale.configure do |config|
-    if (host = ENV["JUDOSCALE_HOST"])
-      # Send reports to a custom host for testing
-      original_url = ENV["JUDOSCALE_URL"] || "https://adapter.judoscale.com/api"
-      original_url += "/#{ENV["RENDER_SERVICE_ID"]}" if ENV["RENDER_SERVICE_ID"]
+    # Send reports to a custom host for testing
+    host = ENV["JUDOSCALE_HOST"]
 
+    original_url = if (render_service_id = ENV["RENDER_SERVICE_ID"])
+      "https://adapter.judoscale.com/api/#{render_service_id}"
+    elsif (url_env_key = ENV.keys.grep(/JUDOSCALE.*_URL/).first)
+      ENV[url_env_key]
+    end
+
+    if host && original_url
       uri = URI.parse(original_url)
       uri.host = host
 
