677aebfeef3400b362403468292b8db863ff6124
Adam McCrea <adam@adamlogic.com>
2023-10-18 10:35:14 -0400

    Use with judoscale-staging add-on

--- config/initializers/judoscale.rb
+++ config/initializers/judoscale.rb
@@ -5,15 +5,18 @@ if defined?(Judoscale)
     # Send reports to a custom host for testing
     host = ENV["JUDOSCALE_HOST"]
 
+    # Use with judoscale or judoscale-staging addons
+    env_var_key = ENV.keys.grep(/JUDOSCALE.*_URL/).first
+
     original_url = if (render_service_id = ENV["RENDER_SERVICE_ID"])
       "https://adapter.judoscale.com/api/#{render_service_id}"
-    elsif (url_env_key = ENV.keys.grep(/JUDOSCALE.*_URL/).first)
-      ENV[url_env_key]
+    elsif env_var_key
+      ENV[env_var_key]
     end
 
-    if host && original_url
+    if original_url
       uri = URI.parse(original_url)
-      uri.host = host
+      uri.host = host if host
 
       config.api_base_url = uri.to_s
     end
